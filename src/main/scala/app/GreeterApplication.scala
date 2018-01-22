ac21b5c4a57068d5014ae563e6aa113d0a4894a7
Andrew Armstrong <andrew.armstrong@digital.hmrc.gov.uk>
2018-01-22 11:37:39 +0000

    Greeting updated "What is your name"

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -1,9 +1,16 @@
 package app
 
+import scala.io.StdIn
+
 /**
   * Created by digital001832 on 22/01/18.
   */
 object GreeterApplication extends App {
-  println("Hello!")
+
+  def greet(name : String) : Unit = println(s"Hello $name")
+
+  val name = StdIn.readLine("What is your name? ")
+
+  greet(name)
 
 }
