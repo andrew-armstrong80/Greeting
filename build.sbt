6be2b0901b8e5c4cd47b26fddb1cfb01672216aa
Andrew Armstrong <andrew.armstrong@digital.hmrc.gov.uk>
2018-01-22 11:06:24 +0000

    New Greetings

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "Greeting"
+
+version := "1.0"
+
+scalaVersion := "2.12.4"
+    
---
+++ src/main/scala/app/GreeterApplication.scala
@@ -0,0 +1,9 @@
+package app
+
+/**
+  * Created by digital001832 on 22/01/18.
+  */
+object GreeterApplication extends App {
+  println("Hello!")
+
+}
