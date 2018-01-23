bf1463820d1d24d4a56715f3e12708e8752e38b3
Andrew Armstrong <andrew.armstrong@digital.hmrc.gov.uk>
2018-01-23 15:51:00 +0000

    Bank Account added

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -2,6 +2,28 @@ package app
 
 import scala.io.StdIn
 
+
+abstract class BankAccount(accountNumber : String,
+                           balance: Double) {
+
+  def withdraw(amount : Double) : BankAccount
+  def deposit(amount : Double) : BankAccount
+}
+
+class SavingsAccount(accountNumber: String, balance : Double) extends BankAccount(accountNumber, balance) {
+
+  override def deposit(amount: Double) : BankAccount = new SavingsAccount (accountNumber, balance + amount)
+
+  override def withdraw(amount: Double) : BankAccount = new SavingsAccount (accountNumber, balance - amount)
+}
+
+class CashISAAccount(accountNumber: String, balance : Double) extends BankAccount(accountNumber, balance)  {
+
+  override def deposit(amount: Double) : BankAccount =  new CashISAAccount(accountNumber, balance + amount)
+
+  override def withdraw(amount: Double) : BankAccount = new CashISAAccount(accountNumber, balance - amount)
+}
+
 class Person(name : String, age : Int) {
 
   private val years : String = if(age==1) "year" else "years"
@@ -15,12 +37,16 @@ class Person(name : String, age : Int) {
   }
 }
 
+
 object Prompt {
 
   def ask(message : String) : String = StdIn.readLine(message)
 }
 
 object GreeterApplication extends App {
+
+  val savingsAccount = new SavingsAccount("12345", 100.00)
+  val savingsPlus100 = savingsAccount.deposit(100.00)
 
   val name = Prompt.ask("What is your name? ")
   val age = Prompt.ask("How old are you? ")
